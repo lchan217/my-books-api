3357962269aeb59c82ce526241ba50c913c26bc1
Lizzie <lchan217@gmail.com>
2019-11-10 08:34:59 -0700

    edit book route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope '/api' do
-    get :books, to: 'books#index'
-    get :books, to: 'books#create'
+    get :books, to: 'book#index'
+    get :books, to: 'book#create'
     get :authors, to: 'author#index'
     post :authors, to: 'author#create'
   end
