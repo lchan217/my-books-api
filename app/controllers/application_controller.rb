94308638c7d7b709bb27f83c45c273328791700d
Lizzie <lchan217@gmail.com>
2019-11-29 12:28:07 -0700

    delete helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
-    helper_method :current_user, :logged_in?
+    # helper_method :current_user, :logged_in?
 
-    #helper methods
-    def current_user
-      @user ||= User.find(session[:user_id]) if logged_in?
-    end
-    def logged_in?
-      !!session[:user_id]
-    end
+    # #helper methods
+    # def current_user
+    #   @user ||= User.find(session[:user_id]) if logged_in?
+    # end
+    # def logged_in?
+    #   !!session[:user_id]
+    # end
 end
