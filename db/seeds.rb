3ea3fff893a3ecf1b252f30fcda7817f73f378ff
Lizzie <lchan217@gmail.com>
2019-11-30 17:56:42 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(id: 1, name: "test1", email: "test1", password: "test1")
+User.create(id: 2, name: "test2", email: "test2", password: "test2")
+User.create(id: 3, name: "test3", email: "test3", password: "test3")
+
+Author.create(id: 1, name: "test1")
+Author.create(id: 2, name: "test2")
+Author.create(id: 3, name: "test3")
+
+Book.create(title: "test1", user_id: 1, author_id: 1)
+Book.create(title: "test2", user_id: 2, author_id: 2)
+Book.create(title: "test3", user_id: 3, author_id: 3)
