4b660032bbeb55c3013519a8a7cba1203e4de638
Lizzie <lchan217@gmail.com>
2019-12-01 12:11:09 -0700

    add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#create these in rails c because it will mess up ID order
 User.create(id: 1, name: "test1", email: "test1", password: "test1")
 User.create(id: 2, name: "test2", email: "test2", password: "test2")
 User.create(id: 3, name: "test3", email: "test3", password: "test3")
@@ -5,7 +6,13 @@ User.create(id: 3, name: "test3", email: "test3", password: "test3")
 Author.create(id: 1, name: "test1")
 Author.create(id: 2, name: "test2")
 Author.create(id: 3, name: "test3")
+Author.create(id: 4, name: "test1 again")
+Author.create(id: 5, name: "test2 again")
+Author.create(id: 6, name: "test3 again")
 
 Book.create(title: "test1", user_id: 1, author_id: 1)
+Book.create(title: "test1 again", user_id: 1, author_id: 4)
 Book.create(title: "test2", user_id: 2, author_id: 2)
+Book.create(title: "test2 again", user_id: 2, author_id: 5)
 Book.create(title: "test3", user_id: 3, author_id: 3)
+Book.create(title: "test3 again", user_id: 3, author_id: 6)
