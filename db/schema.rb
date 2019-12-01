a85cb12d496fef627f42cb5bb3ecefb9a91cc0bc
Lizzie <lchan217@gmail.com>
2019-12-01 16:50:58 -0700

    add author to book table and disable author relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_223659) do
+ActiveRecord::Schema.define(version: 2019_12_01_234839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_11_29_223659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "author"
   end
 
   create_table "users", force: :cascade do |t|
